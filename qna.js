0fab475d03679c54f5536e351a4dcb3751fc2d71
sunnythelucky <sunnyparkk@gmail.com>
2021-08-06 14:33:49 -0400

    first commit

---
+++ qna.js
@@ -0,0 +1,169 @@
+/*
+ 'ISFJ' 0 Russian Blue
+ 'INFP' 1 Cham
+ 'INFJ' 2 Persian
+ 'ESTP' 3 Bengal Turkish
+ 'ENFJ' 4 Turkish Angora
+ 'ESFJ' 5 Arbisinian
+ 'INTP' 6 Norway Forest
+ 'ESFP' 7 Amrerican Shorthair
+ 'ENFP' 8 Korean Shorthair
+ 'ESTJ' 9 Exotic
+ 'ISFP' 10 Scotish Fold
+ 'ENTJ' 11 Munchicken
+ 'ISTJ' 12 Reckdol
+ 'ISTP' 13 British Shorthair
+ 'ENTP' 14 Spinx
+ 'INTJ' 15 Main Khun
+*/
+
+const qnaList = [
+	{
+		q: "When you start<br>working in the new company!",
+		a: [
+			{ answer: "Adept to the new environment easily.", type: [4, 8, 11, 14, 5, 7, 9, 3] },
+			{ answer: "Waiting for someone who will say hi.", type: [2, 1, 15, 6, 0, 10, 12, 13] },
+		],
+	},
+	{
+		q: "Is there anyone who wants to make a presentation?",
+		a: [
+			{ answer: "Me ! Me !", type: [1, 5, 4, 3, 9, 7, 8, 14, 11] },
+			{ answer: "Please don't look at me...", type: [13, 10, 6, 2, 15, 12, 0] },
+		],
+	},
+	{
+		q: "Let's go to the vacay to the UK",
+		a: [
+			{ answer: "We are going to do an activity<br> first.. and then", type: [4, 11, 5, 9, 2, 15, 0, 12] },
+			{ answer: "No plan is the best plan!", type: [8, 14, 7, 3, 1, 6, 10, 13] },
+		],
+	},
+	{
+		q: "You got a great offer<br>to leave the country where you live.",
+		a: [
+			{ answer: "There are too many people behind...", type: [2, 0, 15, 7, 9, 11, 12, 4, 5, 0, 6] },
+			{ answer: "Couldn't be better! Let's go!", type: [8, 1, 7, 3, 13, 14] },
+		],
+	},
+	{
+		q: "I can not stand the<br>annoying friend anymore 😡",
+		a: [
+			{ answer: "This is the last time (500th..)", type: [12, 9, 5, 1, 8, 2, 4, 0, 10, 6] },
+			{ answer: "Cut ties. PERIOD.", type: [11, 15, 14, 13, 7, 3] },
+		],
+	},
+
+	{
+		q: 'When you say "Dope!"',
+		a: [
+			{ answer: "LITERALLY !!! It's so fxxxing dope folks!!!", type: [4, 8, 11, 14, 5, 7, 9, 3] },
+			{ answer: "Just a habitual expression", type: [2, 1, 15, 6, 0, 10, 12, 13] },
+		],
+	},
+	{
+		q: "I ran into the clothes that I wanted to get",
+		a: [
+			{ answer: "DESTINY, Go get it!", type: [14, 8, 3, 7, 6, 1, 13, 10] },
+			{ answer: "Just pass by it", type: [11, 12, 0, 9, 15, 2, 4, 5] },
+		],
+	},
+	{
+		q: "Grade is not as good as<br>I thought",
+		a: [
+			{ answer: "I worked hard, it means a lot", type: [4, 8, 5, 7, 2, 1, 0, 10] },
+			{ answer: "I am so angry and upset", type: [11, 14, 9, 3, 15, 6, 12, 13] },
+		],
+	},
+	{
+		q: '"I am having trouble<br>getting a job...😥"',
+		a: [
+			{ answer: "Let me see your resume", type: [11, 14, 9, 3, 15, 6, 12, 13] },
+			{ answer: "I know how you feel🥲", type: [4, 8, 5, 7, 2, 1, 0, 10] },
+		],
+	},
+	{
+		q: "At my high school reunion",
+		a: [
+			{ answer: "Guess what happened to me! ", type: [4, 8, 11, 14, 5, 7, 9, 3] },
+			{ answer: "(NOD) Listening...", type: [2, 1, 15, 6, 0, 10, 12, 13] },
+		],
+	},
+	{
+		q: "When it comes to a new challenge",
+		a: [
+			{ answer: "I've never tried it before<br>Am I able to do it well?...", type: [5, 7, 9, 3, 0, 10, 12, 13] },
+			{ answer: "Let's do it! Nothing is impossible! 🤩̑", type: [4, 8, 11, 14, 2, 1, 15, 6] },
+		],
+	},
+	{
+		q: "When you have an exam next week",
+		a: [
+			{ answer: "let's start with math<br>and then science tomorrow...", type: [4, 11, 5, 9, 2, 15, 0, 12] },
+			{ answer: "Chill out, There are still more 7 days left YAY🥳", type: [8, 14, 7, 3, 1, 6, 10, 13] },
+		],
+	},
+];
+
+const infoList = [
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+	{
+		name: "f ",
+		desc: " hsgf",
+	},
+];
